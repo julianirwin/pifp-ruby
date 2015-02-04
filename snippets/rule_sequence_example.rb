601538f224486d70829c5a46008da1b8d6c2e383
Julian Irwin <julian.irwin@gmail.com>
2015-02-04 16:09:20 -0600

    finished - should be rubified though

--- snippets/rule_sequence_example.rb
+++ snippets/rule_sequence_example.rb
@@ -1,19 +1,3 @@
-# def zero(s):
-#     if s[0] == "0":
-#         return s[1:]
-
-# def one(s):
-#     if s[0] == "1":
-#         return s[1:]
-
-# def rule_sequence(s, rules):
-#     for rule in rules:
-#         s = rule(s)
-#         if s == None:
-#             break
-
-#     return s
-
 def zero(s)
   if s[0] == "0"
     s[1..-1]
@@ -21,22 +5,25 @@ def zero(s)
 end
 
 def one(s)
-  if s[1] == "1"
+  if s[0] == "1"
     s[1..-1]
   end
 end
 
 def rule_sequence(s, rules)
   rules.each do |rule|
-    s = rule(s)
+    s = rule[s]
     break if s.nil?
   end
+  return s
 end
 
+# There MUST be a ruby way to do this functionally. This way is crap.
+zero = method :zero
+one = method :one
 
-
-# p rule_sequence("0101", [zero, one, zero])
+p rule_sequence("0101", [zero, one, zero])
 # => 1
 
-# p rule_sequence("0101", [zero, zero])
-# => None
+p rule_sequence("0101", [zero, zero])
+# => nil
